1768f97dc50a12392599930b1715f356b8d48cb4
x2r40 <mrx21214@gmail.com>
2025-10-30 21:25:22 +0100

    feat: Add Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 
 import React, { useState, useEffect } from 'react';
+import { Analytics } from '@vercel/analytics/react';
 import { Preloader } from './components/Preloader';
 import { Header } from './components/Header';
 import { FeaturedApps } from './components/FeaturedApps';
@@ -53,6 +54,7 @@ const App: React.FC = () => {
         <Footer />
         <DownloadModal game={selectedGame} onClose={handleCloseModal} />
       </div>
+      <Analytics />
     </>
   );
 };
